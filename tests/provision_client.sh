cdbad21e7709415ae7acea2215bc48d6192cafba
dalesit <joel+github@dalesit.com>
2016-02-09 09:44:56 +0000

    Changed to use Puppet4

--- tests/provision_client.sh
+++ tests/provision_client.sh
@@ -3,9 +3,9 @@
 # Installs appropriate packages for Vagrant vms
 
 # apt-get install -y python-software-properties
-wget --quiet http://apt.puppetlabs.com/puppetlabs-release-precise.deb -O /tmp/puppetlabs-release-precise.deb
-dpkg -i /tmp/puppetlabs-release-precise.deb
+wget --quiet https://apt.puppetlabs.com/puppetlabs-release-pc1-trusty.deb -O /tmp/puppetlabs-release-pc1-trusty.deb
+dpkg -i /tmp/puppetlabs-release-pc1-trusty.deb
 apt-get update
-apt-get install -y ruby-json puppet-common ruby-dev #masterless puppet
+apt-get install -y ruby-json puppet-agent ruby-dev #masterless puppet
 sed -i '/templatedir/d' /etc/puppet/puppet.conf
 puppet module install sensu/sensu
